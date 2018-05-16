b079205abdacf6eb0fc3e8c8de062067a3180b5a
madsnk <mads.nielsen.kjaersgaard@gmail.com>
2018-05-16 10:13:24 +0200

    Add bash script for CUDA install

---
+++ bashscripts/cuda_install.sh
@@ -0,0 +1,34 @@
+#First update and install pip, virtualenv etc.
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install pip
+export LC_ALL=C
+echo 'export LC_ALL=C' >> ~/.bashrc 
+pip install --upgrade pip
+pip install virtualenv
+virtualenv -p python3 venv
+. venv/bin/activate
+pip install jupyter
+
+#Download and install cuda 9.0 and cudnn 7.0
+#https://yangcha.github.io/CUDA90/
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libnccl2_2.1.4-1+cuda9.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libnccl-dev_2.1.4-1+cuda9.0_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+sudo dpkg -i libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
+sudo dpkg -i libnccl2_2.1.4-1+cuda9.0_amd64.deb
+sudo dpkg -i libnccl-dev_2.1.4-1+cuda9.0_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda=9.0.176-1
+sudo apt-get install -y libcudnn7-dev
+sudo apt-get install -y libnccl-dev
+rm *.deb
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+echo 'export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}' >> ~/.bashrc 
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc 
