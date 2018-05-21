942b7f2f8015bf52abefe01adae8cc4d7916bf77
madsnk <mads.nielsen.kjaersgaard@gmail.com>
2018-05-21 11:23:55 +0000

    Notebook changes

--- deepMasking/finlandsgade.py
+++ deepMasking/finlandsgade.py
@@ -108,7 +108,6 @@ class FinlandsgadeDataset(utils.Dataset):
         # Add all coco classes so data fits with pretrained COCO model
         #class_indice = 1
         for i in range(1, 81):
-            print(i)
             self.add_class("finlandsgade", i, class_names[i])
 
         # Train or validation dataset?
